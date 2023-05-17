3f6671b4058d4c10635ea9c7c19dca4d3d96181a
Roman <i027356@inbox.ru>
2023-05-17 07:43:24 +0700

    first commit

---
+++ js/accordeon.js
@@ -0,0 +1,19 @@
+let accordeon = document.querySelectorAll('.accordeon')
+let accText = document.querySelectorAll('.acctext')
+
+accordeon.forEach((el) => {
+  let img = el.querySelector('.btnimg')
+  el.addEventListener('click', () => {
+    let content = el.nextElementSibling
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null
+      img.src = '/img/expand_more_FILL0_wght400_GRAD0_opsz48.svg'
+    } else {
+      accText.forEach((el) => {
+        el.style.maxHeight = null
+      })
+      content.style.maxHeight = content.scrollHeight + 'px'
+      img.src = '/img/expand_less_FILL0_wght400_GRAD0_opsz48.svg'
+    }
+  })
+})
